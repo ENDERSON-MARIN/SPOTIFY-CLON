51bbd0ed1d156860d484212e0637c8f395229603
ENDERSON MARIN <marinenderson1@gmail.com>
2022-04-17 15:00:06 -0500

    initial commit-login frontend ready!

---
+++ src/components/login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoginButton, LoginContainer } from "./styles";
+
+const Login = () => {
+  return (
+    <LoginContainer>
+      <img src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-symbol.jpg" alt="logo" />
+      <LoginButton href="#">Login with spotify</LoginButton>
+    </LoginContainer>
+  );
+};
+
+export default Login;
